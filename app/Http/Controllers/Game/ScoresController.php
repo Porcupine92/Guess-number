e67ebd03f9a4db121c88ecbb5b5b46a3eed949cb
Porcupine92 <szymonjozefowicz1992@gmail.com>
2021-01-14 21:42:13 +0100

    Game branch

--- app/Http/Controllers/Game/ScoresController.php
+++ app/Http/Controllers/Game/ScoresController.php
@@ -10,11 +10,6 @@ class ScoresController extends Controller
 {
     public function __invoke(): JsonResponse
     {
-        // Tutaj pobieramy wszytskie wyniki użytkowników
-        // Sprawdzamy ilość tych wyników. Jeśli ich ilość jest większa od 30,
-        // ograniczamy wyniki do 30 najwyższych i je wyświetlamy jsonem
-        // w postaci tablicy gdzie każdy rekord to nazwa użytkownika i jego wynik
-
         $data = [];
 
         $allGames = Game::orderBy('score', 'desc')
@@ -24,8 +19,6 @@ class ScoresController extends Controller
         foreach ($allGames as $game) {
             $data[] = ['playerName' => $game->playerName, 'score' => $game->score];
         }
-
-        // dd($allGames);
 
         return response()->json($data);
     }
